2499372791a750c8c622de1ed2eb9f828de36e35
elgarno <faffi@gmx.de>
2021-04-25 17:43:04 +0200

    changed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-mkdir -p ~/.streamlit
-echo “[server]
-headless = true
-port = $PORTenableCORS = false
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"faffi@gmx.de\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
